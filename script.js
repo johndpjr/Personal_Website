735834ab6ef51fd68b10e278afafa3d0785103d7
john <johndpowell02@gmail.com>
2023-02-06 15:40:03 -0600

    implement theme switch

---
+++ script.js
@@ -0,0 +1,21 @@
+function switchTheme() {
+    themeElem = document.getElementById('style');
+
+    if (themeElem.getAttribute('href') == 'matrix-style.css') {
+        themeElem.setAttribute('href', 'oj-blue-style.css');
+    } else {
+        themeElem.setAttribute('href', 'matrix-style.css');
+    }
+
+    localStorage.setItem('style', themeElem.getAttribute('href'));
+}
+
+
+window.onload = function() {
+    themeName = localStorage.getItem('style');
+    if (!themeName) {
+        return;
+    }
+    themeElem = document.getElementById('style');
+    themeElem.setAttribute('href', themeName);
+}
